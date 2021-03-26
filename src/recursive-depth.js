5cce704e37bef745bc4ed31452b28560541a82d4
BuildTools <unconfigured@null.spigotmc.org>
2021-03-27 00:17:19 +0400

    feat: add recursive-depth solution

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -2,14 +2,10 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
   calculateDepth(arr) {
+    let self = this;
     return Array.isArray(arr) ? 1 + arr.reduce(function(a, b) {
-      return Math.max(a, calculateDepth(b));
+      return Math.max(a, self.calculateDepth(b));
     }, 0) 
     : 0;
   } 
 };
-
-//if (Array.isArray(arr)) {
-//  return 1 + Math.max(...arr.map(t => this.calculateDepth(t)))
-//  }
-//  else return 0;
