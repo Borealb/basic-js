805600ad8a5a5587c23d5b9dcd9fca2d631f1d5e
BuildTools <unconfigured@null.spigotmc.org>
2021-03-26 23:43:38 +0400

    feat: add dream-team solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,14 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(members) {
-  let team = [];
-  for (let i = 0; i < members.length; i++) {
-    if (members.forEach(typeof(members) == 'string')) {
-     team.push(members.trim())
-  };
-};
   if(Array.isArray(members) === false) {
     return false;
   }
-  return team.toUpperCase().split().sort().join('');
+  let team = [];
+  for (let i = 0; i < members.length; i++) {
+    if (typeof(members[i]) === 'string') {
+     team.push(members[i].trim().charAt(0).toUpperCase())
+  };
+};
+  return team.sort().join('')
 }
