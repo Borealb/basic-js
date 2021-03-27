347d9a3eed2ed56e63ca78e406bd9af3a16b26ff
Vladislav Tur <vla-tur@yandex.ru>
2021-03-27 19:16:21 +0400

    feat: start simple-chain
    	feat:  'start simple-chain'

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,14 +1,23 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chain = [],
+
   getLength() {
-    
+    return this.chain.length;
   },
   addLink(value) {
-    
+     this.chain.push(`'(${value})'`)
+     return this;
   },
   removeLink(position) {
-    
+    if (typeof(position) === 'number') {
+      this.chain.splice(`${position}`,1);
+    } else if (typeof(position) !== 'number') {
+      return ('Error')
+    } else if (this.getLength < position) {
+      return ('Error')
+    } 
   },
   reverseChain() {
     
